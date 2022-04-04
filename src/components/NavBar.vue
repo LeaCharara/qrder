d0187050ea13f40038419075e9890b0fbc8c4857
saif <saif.trabelsi.perso@gmail.com>
2022-04-04 11:29:21 -0400

    Update NavBar.vue
    icons are in grey
    active icon is in white

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,28 +1,28 @@
   <template>
   <v-bottom-navigation 
   v-model="value" 
-    color="blue"
+    color="white"
     style="background-color:black"
     >
-    <v-btn style="color:white" @click="value='Catalog'" to="/"  value="Catalog">
+    <v-btn style="color:grey" @click="value='Catalog'" to="/"  value="Catalog">
       <v-icon>mdi-food</v-icon>
       <span>Catalog</span>
 
     </v-btn>
 
-    <v-btn style="color:white" to="/scan" @click="value='Scan'"    value="Scan">
+    <v-btn style="color:grey" to="/scan" @click="value='Scan'"    value="Scan">
       <v-icon>mdi-qrcode</v-icon>
       <span>Scan</span>
 
     </v-btn>
 
-    <v-btn style="color:white" to="/orders" @click="value='Orders'" value="Orders">
+    <v-btn style="color:grey" to="/orders" @click="value='Orders'" value="Orders">
       <v-icon>mdi-history</v-icon>
             <span>Orders</span>
 
     </v-btn>
 
-    <v-btn style="color:white" @click="value='Profile'"  value="Profile">
+    <v-btn style="color:grey" @click="value='Profile'"  value="Profile">
       <v-icon>mdi-account</v-icon>
             <span>Profile</span>
 
